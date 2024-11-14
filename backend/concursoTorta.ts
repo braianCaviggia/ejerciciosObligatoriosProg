b9b381b1caccda79ae2c8c70e1252a96af81d0be
braianCaviggia <braiancaviggia10@gmail.com>
2024-11-13 21:23:30 -0300

    Ejercicios Obligatorios de TypeScript

---
+++ backend/concursoTorta.ts
@@ -0,0 +1,39 @@
+import * as rs from 'readline-sync';
+
+let calcularPuntajes = (sabor:number,presentacion:number,dificultad:number):number => {
+    return sabor+presentacion+dificultad
+}
+
+let determinarGanador = ():void => {
+    let cantidadConcursantes : number = rs.questionInt("Ingresa la cantidad de concursantes: ");
+    let puntajeMasAlto:number = 0
+    let empate = false
+    
+    for (let i = 1; i<=cantidadConcursantes;i++) {
+        let sabor:number = rs.questionInt(`Ingresa la puntuacion del sabor del concursante ${i} del 1 al 5: `);
+        let presentacion:number = rs.questionInt(`Ingresa la puntuacion de la presentacion del concursante ${i} del 1 al 5: `);
+        let dificultad:number = rs.questionInt(`Ingresa la puntuacion de la dificultad del concursante ${i} del 1 al 5: `);
+
+        if ((sabor >= 1 && sabor <= 5)&&(presentacion >=1 && presentacion <=5)&& (dificultad >=1 && dificultad <=5)) {
+        } else {
+            console.log("¡¡La puntuacion es del 1 al 5!!");
+            break;
+        } 
+
+        let puntaje = calcularPuntajes(sabor,presentacion,dificultad);
+        console.log(`El puntaje del concursante ${i} es de ${puntaje}`) 
+
+        if (puntaje > puntajeMasAlto) {
+            puntajeMasAlto = puntaje
+        } else if (puntaje == puntajeMasAlto) {
+            empate = true
+        }
+} 
+if (empate) {
+    console.log("Hay empate")
+} else {
+    console.log(`El ganador obtuvo un total de ${puntajeMasAlto}`)
+}
+
+} 
+determinarGanador()
